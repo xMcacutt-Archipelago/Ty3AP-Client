73c1e99595c05b5a6f1a0feb3006a185cc971be7
Matthew Cacutt <matthew.cacutt@gmail.com>
2026-04-10 17:46:12 +0100

    v1.2.1

--- Ty3AP-Client/Version.h
+++ Ty3AP-Client/Version.h
@@ -1,4 +1,4 @@
 #pragma once
-#define VERSION_STRING "1.1.0"
-#define COMPAT_VERSION_STRING "1.1.0"
+#define VERSION_STRING "1.2.1"
+#define COMPAT_VERSION_STRING "1.2.0"
 #define GAME_NAME "Ty the Tasmanian Tiger 3"
